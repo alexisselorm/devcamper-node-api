ba1504b58937e82157f44ffe98b551ba7c795644
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-06-24 20:57:46 +0200

    Routing and controllers

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express= require('express');
 const dotenv = require('dotenv');
 
+//Routes
+const bootcamps = require('./routes/bootcamps');
+
+
 dotenv.config({path:'./config.env'});
 
 const app = express();
+
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+
+app.get('/', (req,res)=>{
+  res.status(400).json({data:"Hello from Express"})
+})
+
 
 PORT = process.env.PORT || 5000;
 
