8675dfb2d37d10b72d8d437b154e5bae86fffef5
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-06-24 21:31:50 +0200

    Understanding Middlewares

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express= require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger.middleware');
+const morgan = require('morgan');
 
 //Routes
 const bootcamps = require('./routes/bootcamps');
@@ -8,6 +10,12 @@ const bootcamps = require('./routes/bootcamps');
 dotenv.config({path:'./config.env'});
 
 const app = express();
+
+//Dev logging middeware
+if (process.env.NODE_ENV=='development') {
+  app.use(morgan('dev'))
+}
+
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
