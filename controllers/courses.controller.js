443b424895ac422f41d8683f6750e0f3eedde544
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-06-28 22:03:38 +0200

    Full CRUD for courses

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -50,3 +50,40 @@ exports.createCourse = asyncHandler(async(req,res,next)=>{
   res.status(200).json({success:true,count:course.length,data:course})
 })
 
+
+//@desc Update course
+//@route [PUT] /api/v1/courses/:id
+//@access private
+exports.updateCourse = asyncHandler(async(req,res,next)=>{
+  
+  const course = await Course.findById(req.params.id)
+
+  
+  if (!course) {
+    return next(new ErrorResponse(`No course found with the id ${req.params.id}`))
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id,req.body,{
+    new:true,
+    runValidators:true
+  });
+  res.status(200).json({success:true,data:course})
+})
+
+
+//@desc Update course
+//@route [PUT] /api/v1/courses/:id
+//@access private
+exports.deleteCourse = asyncHandler(async(req,res,next)=>{
+  
+  const course = await Course.findById(req.params.id)
+
+  
+  if (!course) {
+    return next(new ErrorResponse(`No course found with the id ${req.params.id}`))
+  }
+
+  await Course.deleteOne();
+  res.status(200).json({success:true,data:{}})
+})
+
--- routes/courses.js
+++ routes/courses.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getCourses, getCourse,createCourse } = require('../controllers/courses.controller');
+const { getCourses, getCourse,createCourse, updateCourse, deleteCourse } = require('../controllers/courses.controller');
 
 
 const router = express.Router({mergeParams:true});
@@ -10,10 +10,10 @@ router.route('/')
 
 router.route('/:id')
 .get(getCourse)
+.put(updateCourse)
+.delete(deleteCourse);
 // .post(createBootcamp);
 
 // router.route('/:id')
 // .get(getBootcamp)
-// .put(updateBootcamp)
-// .delete(deleteBootcamp);
 module.exports = router
