494ffb3cf90d0006cc8b462c20316bf3ed984a73
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-06-29 02:44:26 +0200

    Register / Login basic

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,17 +3,50 @@ const ErrorResponse = require('../utils/errorHandler');
 const asyncHandler = require('../middleware/asyncHandler.middleware');
 
 // @desc Register user
-//@route GET /api/v1/auth/register
+//@route POST /api/v1/auth/register
 //@access Public
-exports.register =asyncHandler(async(req,res,next)=>{
+exports.register = asyncHandler(async (req, res, next) => {
 
-  const {name,email,password,role}=req.body;
+  const { name, email, password, role } = req.body;
 
   //Create user
   const user = await User.create({
-    name,email,password,role,
+    name, email, password, role,
   });
 
-  res.status(200).json({success:true, message:"User created successfully"});
+  //Create token
+  const token = user.getSignedJWT();
+
+  res.status(200).json({ success: true, message: "User created successfully", token });
+})
+
+
+
+// @desc Register user
+//@route POST /api/v1/auth/register
+//@access Public
+exports.login = asyncHandler(async (req, res, next) => {
+
+  const { email, password } = req.body;
+
+  //Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide email and password', 400))
+  }
+
+  //Check for user
+  //Create user
+  const user = await User.findOne({ email }).select('+password');
+  if (!user) return next(new ErrorResponse('Invalid credentials', 401))
+
+  //Match password to hashed password
+  const isMatch = await user.matchPassword(password);
+  if(!isMatch) return next(new ErrorResponse('Invalid credentials',401));
+
+
+  //Create token
+  const token = user.getSignedJWT();
+
+  res.status(200).json({ success: true, message: "Login successful", token });
 })
 
