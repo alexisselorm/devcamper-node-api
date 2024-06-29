d52013530e299edc79a26ddcb34f4b75ed627b76
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-06-29 09:49:20 +0200

    Auth middleware

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('./asyncHandler.middleware')
+const ErrorResponse = require('../utils/errorHandler')
+const User = require('../models/user.model')
+
+
+//Protect routes
+
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.token) {
+    token = req.cookies.token
+  }
+
+  //Make sure token exists
+  if(!token){
+    return next(new ErrorResponse('Not Authorized to access this route',401))
+  }
+
+  //Verify token
+  try{
+    let decoded = jwt.verify(token,process.env.JWT_SECRET_KEY);
+  
+    console.log(decoded);
+
+    req.user = await User.findById(decoded.id);
+
+  }catch(e){
+
+  }
+})
