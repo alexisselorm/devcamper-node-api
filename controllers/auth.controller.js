6853f74936b8f7a467256b702a731a823e594ce4
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-07-01 09:49:11 +0200

    Logout

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -204,3 +204,16 @@ exports.updatePassword = asyncHandler(async(req,res,next) => {
 
 
 })
+
+
+// @desc Log out
+//@route GET /api/v1/auth/logout
+//@access Private
+exports.logout = asyncHandler(async(req,res,next) => {
+  res.cookie('token','none',
+    {expires:new Date(Date.now()+10*1000),
+      httpOnly:true
+    })
+  res.status(200).json({ sucess:true,data:{}});
+
+})
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const {register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword} = require('../controllers/auth.controller');
+const {register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword, logout} = require('../controllers/auth.controller');
 const { protect } = require('../middleware/auth.middleware');
 
 const router = express.Router();
 
 router.post('/register', register)
 router.post('/login', login)
+router.get('/logout', logout)
 router.put('/update-details',protect, updateDetails)
 router.put('/update-password',protect, updatePassword)
 router.get('/me', protect, getMe)
