ba341795e310e595aa45067765866b7cde54ea70
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-07-01 06:50:39 +0200

    Update details and password

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const {register, login, getMe, forgotPassword, resetPassword} = require('../controllers/auth.controller');
+const {register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword} = require('../controllers/auth.controller');
 const { protect } = require('../middleware/auth.middleware');
 
 const router = express.Router();
 
 router.post('/register', register)
 router.post('/login', login)
+router.put('/update-details',protect, updateDetails)
+router.put('/update-password',protect, updatePassword)
 router.get('/me', protect, getMe)
 router.post('/forgot-password', forgotPassword)
 router.put('/resetPassword/:resettoken', resetPassword)
